fac1a6f65ca3e3f0a7d9950a8da8c11329da7c3e
ZeroWave022 <36341766+ZeroWave022@users.noreply.github.com>
2024-03-29 18:28:29 +0100

    feat!: Rewrite functions to classes

--- tipcc/tipcc.py
+++ tipcc/tipcc.py
@@ -1,80 +1,47 @@
 import requests
-import json
+import re
+from .balances import BalancesManager
+from .currencies import CurrenciesManager
+from .info import InfoManager
+from .user import User
 
+class TipccClient:
+    def __init__(self, token) -> None:
+        if not re.match(r"(^[A-Za-z0-9-_]*\.[A-Za-z0-9-_]*\.[A-Za-z0-9-_]*$)", token):
+            raise ValueError("Invalid auth token provided")
 
-def init(token):
-    global __TIPCCTOKEN__
-    try:
-        if __TIPCCTOKEN__ is None:
-            if check_token(token) != True:
-                print(
-                    "Your tip.cc auth token is invalid! Make sure to fix this if you want access to the api."
-                )
-                return False
-            else:
-                __TIPCCTOKEN__ = token
-                return True
-        else:
-            print(
-                "Your tip.cc auth token is invalid! Make sure to fix this if you want access to the api."
+        self.token = token
+        self.balances = BalancesManager(token)
+        self.currencies = CurrenciesManager(token)
+        self.info = InfoManager(token)
+        self.user = User(token)
+
+    @classmethod
+    def get_token_from_dms(cls, discord_bot_token):
+        try:
+            headers = {
+                "Content-Type": "application/json",
+                "Authorization": f"Bot {discord_bot_token}"
+            }
+            dm_channel = requests.post(
+                "https://discord.com/api/users/@me/channels",
+                data='{"recipient_id":"350270174462607360"}',
+                headers=headers,
+                timeout=15
             )
-            return False
-    except:
-        if check_token(token) != True:
-            print(
-                "Your tip.cc auth token is invalid! Make sure to fix this if you want access to the api."
+            dm_channel_id = dm_channel.json()["id"]
+            messages_response = requests.get(
+                f"https://discord.com/api/channels/{dm_channel_id}/messages",
+                headers=headers,
+                timeout=15
             )
-            return False
-        else:
-            __TIPCCTOKEN__ = token
-            return True
-
-
-def get_token():
-    return __TIPCCTOKEN__
-
-
-def check_token(tipcc_token):
-    x = requests.get(
-        f"https://api.tip.cc/api/v0/user",
-        headers={
-            "accept": "application/json",
-            "Authorization": f"Bearer {tipcc_token}",
-        },
-    )
-    if x.status_code not in [200, 404, 401]:
-        return False, x.status_code
-    if x.status_code == 401:
-        return False
-    return True
-
-
-def get_token_from_dms(discord_bot_token):
-    try:
-        headers = {
-            "Content-Type": "application/json",
-            "Authorization": "Bot " + discord_bot_token,
-        }
-        dm_response = requests.post(
-            "https://discord.com/api/users/@me/channels",
-            data='{"recipient_id":"350270174462607360"}',
-            headers=headers,
-        )
-        dm = dm_response.json()
-        dms_response = requests.get(
-            "https://discord.com/api/channels/" + dm["id"] + "/messages",
-            headers=headers,
-        )
-        messages = dms_response.json()
-        key = [
-            msg
-            for msg in messages
-            if msg["content"].startswith("eyJ")
-            and msg["author"]["id"] == "350270174462607360"
-        ][-1]
-        if check_token(key["content"]) == True:
+            messages = messages_response.json()
+            key = [
+                msg
+                for msg in messages
+                if msg["content"].startswith("eyJ")
+                and msg["author"]["id"] == "350270174462607360"
+            ][-1]
             return key["content"]
-        else:
-            return "Found key not valid", key["content"]
-    except:
-        return "No api key found"
+        except requests.exceptions.RequestException as exc:
+            raise RuntimeError("Failed retriving the auth token.") from exc
